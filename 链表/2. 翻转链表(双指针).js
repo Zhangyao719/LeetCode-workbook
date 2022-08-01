50db384ec134a68629d071bb02eaf81ceaa7c123
Zhangyao719 <1258858403@qq.com>
2022-08-01 23:39:39 +0800

    feat: 翻转链表

---
+++ 链表/2. 翻转链表(双指针).js
@@ -0,0 +1,21 @@
+/**
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if(!head || !head.next) return head;
+    let temp = null, pre = null, cur = head;
+    while(cur) {
+        temp = cur.next;
+        cur.next = pre;
+        pre = cur;
+        cur = temp;
+    }
+    // temp = cur = null;
+    return pre;
+};

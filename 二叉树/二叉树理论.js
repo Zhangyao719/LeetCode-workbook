a991816cf199d6e33ff6cf84bfdf276c827fe9f1
Zhangyao719 <1258858403@qq.com>
2022-08-18 23:47:34 +0800

    feat: 二叉树

---
+++ 二叉树/二叉树理论.js
@@ -0,0 +1,16 @@
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val
+  this.left = left === undefined ? null : left
+  this.right = right === undefined ? null : right
+}
+
+/**
+ * * 二叉树的遍历方式
+ * todo 深度优先遍历
+ * ? 前序遍历（递归法，迭代法）
+ * ? 中序遍历（递归法，迭代法）
+ * ? 后序遍历（递归法，迭代法）
+ * 
+ * todo 广度优先遍历
+ * ?层次遍历（迭代法)
+ */

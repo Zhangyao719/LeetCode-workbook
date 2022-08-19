95e9dbc8108ec1f6387df810926a735ab135790c
v_yaoxzhang <v_yaoxzhang@tencent.com>
2022-08-19 09:41:34 +0800

    feat:二叉树的中序遍历

--- 二叉树/1. 深度遍历.js
+++ 二叉树/1. 深度遍历.js
@@ -6,7 +6,6 @@ var preorderTraversal = function (root) {
         if (root === null) return;
 
         res.push(root); // 前序遍历从中间的父节点开始
-
         ergodic(root.left); // 递归左子树
         ergodic(root.right); // 递归右子树
     }
@@ -18,7 +17,19 @@ var preorderTraversal = function (root) {
 
 // * 中序遍历
 var inorderTraversal = function (root) {
+    let res = [];
 
+    function ergodic(root) {
+        if (root === null) return;
+
+        ergodic(root.left); // 中序遍历左子树开始
+        res.push(root);
+        ergodic(root.right);
+    }
+
+    ergodic(root);
 }
 
 // * 后序遍历
+var postorderTraversal = function(root) {
+};

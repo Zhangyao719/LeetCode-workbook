9c450468d16f657931a9e148f3e67804c7e7f474
Zhangyao719 <1258858403@qq.com>
2022-08-20 18:37:07 +0800

    feat: 二叉树的中序迭代遍历

--- 二叉树/2. 深度遍历(迭代).js
+++ 二叉树/2. 深度遍历(迭代).js
@@ -18,3 +18,27 @@ var preorderTraversal = function (root, res = []) {
   return res
 }
 
+// * 中序遍历:
+// 入栈 左 -> 右
+// 出栈 左 -> 中 -> 右
+var inorderTraversal = function (root, res = []) {
+  const stack = []
+  let currNode = root
+
+  while (stack.length || currNode) {
+    console.log('currNode', currNode)
+
+    // 如果有节点，一直往下取左子树（此时不取值）
+    if (currNode) {
+      stack.push(currNode) // 暂存当前节点(方便回退时，再取出来)
+      currNode = currNode.left // 取左子树
+    } else {
+      // 如果节点没有了，则返回上一个节点
+      currNode = stack.pop() // 返回上一节点
+      res.push(currNode.val) // 存中值(符合中序遍历的特点)
+
+      currNode = currNode.right // 取右子树
+    }
+  }
+  return res
+}

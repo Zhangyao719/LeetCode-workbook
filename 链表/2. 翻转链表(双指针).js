6eba4f42dcabcdde7dc841ac60111cb0df07ac3f
Zhangyao719 <1258858403@qq.com>
2022-09-04 20:57:53 +0800

    feat: review reversing ListNode

--- 链表/2. 翻转链表(双指针).js
+++ 链表/2. 翻转链表(双指针).js
@@ -7,15 +7,22 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) {
-    if(!head || !head.next) return head;
-    let temp = null, pre = null, cur = head;
-    while(cur) {
-        temp = cur.next;
-        cur.next = pre;
-        pre = cur;
-        cur = temp;
-    }
-    // temp = cur = null;
-    return pre;
-};
+var reverseList = function (head) {
+  if (!head || !head.next) return head
+  // 1. 声明双指针（当前节点和上个节点）
+  let cur = head
+  let pre = null
+  let temp = null
+  while (cur) {
+    // 保存下个节点（因为待会儿当前cur节点要翻转了）
+    temp = cur.next
+
+    // 2. 将当前节点指针指向上一个节点 （翻转结束）
+    cur.next = pre
+
+    // 3. 节点继续往下走
+    pre = cur // 上个节点往下走
+    cur = temp // 当前节点往下走
+  }
+  return pre
+}
